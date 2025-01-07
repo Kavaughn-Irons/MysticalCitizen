b336a56b906be7894a6629067a8db2165c7e352e
kavaughn-irons <kavaughnawsomeness4@gmail.com>
2025-01-07 11:58:44 -0500

    fix port

--- Server.js
+++ Server.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+
+// Use the Heroku port or fallback to 3000 for local development
+const port = process.env.PORT || 3000;
 
 // Enable CORS
 app.use(cors());
