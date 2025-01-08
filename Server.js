c0dc73048613e90f7d53b513f043e789aeb9c3b2
kavaughn-irons <kavaughnawsomeness4@gmail.com>
2025-01-08 13:28:25 -0500

    update app

--- Server.js
+++ Server.js
@@ -29,3 +29,47 @@ app.get('/openai-key', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+require('dotenv').config({ path: './API.env' });
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+// Use the Heroku port or fallback to 3000 for local development
+const port = process.env.PORT || 3000;
+
+// Enable CORS
+app.use(cors({
+    origin: 'http://localhost:5000',
+    credentials: true,
+    allowedHeaders: ['Authorization', 'Content-Type']
+}));
+
+
+// Serve MysticalCitizen.html at the root URL
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/MysticalCitizen.html');
+});
+
+// Endpoint to send Firebase API key (Protected)
+app.get('/firebase-key', (req, res) => {
+    res.json({ 'apiKey': process.env.FIREBASE_API_KEY });
+});
+
+// Endpoint to send OpenAI API key (Protected)
+app.get('/openai-key', (req, res) => {
+    res.json({ 'apiKey': process.env.OPENAI_API_KEY });
+});
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
